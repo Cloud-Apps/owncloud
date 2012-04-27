301be8b7ac847c6053da4281bca474c60411099e
Troy Topnik <troyt@activestate.com>
2012-04-27 16:11:22 -0700

    Workaround for "user" vs. "group" key change.

--- config/autoconfig.php
+++ config/autoconfig.php
@@ -4,10 +4,14 @@ $appinfo = getenv("VCAP_APPLICATION");
 $services_json = json_decode($services,true);
 $appinfo_json = json_decode($appinfo,true);
 $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+if (array_key_exists("users", $appinfo_json))
+  $admin = $appinfo_json["users"][0]; 
+else
+  $admin = $appinfo_json["group"];
 
 $AUTOCONFIG = array(
 "installed" => false,
-"adminlogin" => $appinfo_json["users"][0],
+"adminlogin" => $admin,
 "adminpass" => "changeme",
 "directory" => "/app/app/data", 
 "dbtype" => "mysql",
